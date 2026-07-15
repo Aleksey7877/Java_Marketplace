47e29ca70a6b59a29d2a7899b2f6e225a18d5fcb
Aleksey Sidorov <you@example.com>
2026-07-15 19:34:11 +0300

    build: initialize Spring Boot project

---
+++ src/main/java/marketplace_chat_api/MarketplaceChatApiApplication.java
@@ -0,0 +1,13 @@
+package marketplace_chat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceChatApiApplication.class, args);
+	}
+
+}
